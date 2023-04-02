a9dba081d855ecc93b80442f776b6156098089be
박상준 <psjoon@epart.com>
2023-04-02 15:37:01 +0900

    refactor: Main 메서드 refactoring & 파일 이동 복사 기능 구현
    Command.java 를 enum 클래스로 관리하여 클래스안의 value 값에 해당 값이 존재하지 않는다면 iae 에러 발생시키도록 변경
    또한 command switch문으로도 변경함
    
    Author: 박상준

---
+++ src/main/java/linux/sub/main/Command.java
@@ -0,0 +1,18 @@
+package linux.sub.main;
+
+/**
+ * packageName    : linux.sub.main
+ * fileName       : Command
+ * author         : ipeac
+ * date           : 2023-04-02
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-04-02        ipeac       최초 생성
+ */
+public enum Command {
+    CAT,
+    MV,
+    CP
+}
