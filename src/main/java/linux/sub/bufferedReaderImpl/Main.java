1e70047a3c463ff30a5d58711fa1a766adead0eb
박상준 <psjoon@epart.com>
2023-04-04 10:51:06 +0900

    feat: bufferReader readline 구현
    Author: 박상준

--- src/main/java/linux/sub/bufferedReaderImpl/Main.java
+++ src/main/java/linux/sub/bufferedReaderImpl/Main.java
@@ -1,5 +1,9 @@
 package linux.sub.bufferedReaderImpl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * packageName    : linux.sub.bufferedReaderImpl
  * fileName       : Main
@@ -12,5 +16,32 @@ package linux.sub.bufferedReaderImpl;
  * 2023-04-04        ipeac       최초 생성
  */
 public class Main {
-
+    public static void main(String[] args) throws IOException {
+        String s = readLine();
+        System.out.println("s = " + s);
+    }
+    
+    public static String readLine() throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+            char[] buffer = new char[5];
+            while (true) {
+                int len = br.read(buffer);
+                if (len == -1) {
+                    break;
+                }
+                String w = new String(buffer, 0, len);
+                int index = w.indexOf("\n");
+                if (index != -1) {
+                    sb.append(buffer, 0, index);
+                    break;
+                
+                } else {
+                    sb.append(buffer, 0, len);
+                }
+            }
+            return sb.toString();
+        }
+    }
+    
 }
