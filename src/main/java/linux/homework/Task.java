bb29804e5f409e98a6381c7bfa4f0233b9c422e5
박상준 <psjoon@epart.com>
2023-04-16 22:37:14 +0900

    feat: task 클래스만 만듬
    Author: 박상준

---
+++ src/main/java/linux/homework/Task.java
@@ -0,0 +1,20 @@
+package linux.homework;
+
+import java.io.IOException;
+
+/**
+ * packageName    : linux.homework
+ * fileName       : Task
+ * author         : ipeac
+ * date           : 2023-04-16
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-04-16        ipeac       최초 생성
+ */
+public class Task {
+    public static void main(String[] args) throws IOException {
+    
+    }
+}
--- src/main/java/linux/sub/playground/Main3.java
+++ src/main/java/linux/sub/playground/Main3.java
@@ -18,17 +18,26 @@ import java.nio.charset.StandardCharsets;
  */
 public class Main3 {
     public static void main(String[] args) throws IOException {
-        String word = " qijwieqwehqj lqkjdlasjdlj lasjdla lsda";
-        byte[] bytes = word.getBytes(StandardCharsets.UTF_8);
-        InputStream bis = new ByteArrayInputStream(bytes);
-        byte[] buffer = new byte[4];
+        String word = "asdjlaj alsdjal jd asljd la";
+        //논리적 데이터를 물리적 데이터로 변환합니다.
+        byte[] bword = word.getBytes(StandardCharsets.UTF_8);
+        //물리적 데이터가 메모리에 존재하기에 , 메모리 전용 스트림 생성
+        InputStream is = new ByteArrayInputStream(bword);
+        //스트리밍
+        // 1) 적절한 처리단위를 가지고
+        byte[] bytes = new byte[4];
+        // 2) 연속적인 스트림 처리합니다.
         while (true) {
-            int len = bis.read(buffer);
+            //처리단위 크기만큼 스트림을 받습니다.
+            int len = is.read(bytes);
+            // -1 을 반환하는 경우 연속적인 처리를 종료해야합니다.
             if (len == -1) {
                 break;
             }
-            String s = new String(buffer, 0, len, StandardCharsets.UTF_8);
+            //물리적 데이터를 논리적 데이터로 변환합니다.
+            String s = new String(bytes, 0, len, StandardCharsets.UTF_8);
             System.out.print(s);
         }
+        
     }
 }
