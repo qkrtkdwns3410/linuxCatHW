668e01f45524f19cf7cd51200646930f45b27173
박상준 <psjoon@epart.com>
2023-04-17 23:27:37 +0900

    feat: 콘솔입출력까지
    Author: 박상준

---
+++ src/main/java/linux/java/inputstreamrw/StandardIOEx1.java
@@ -0,0 +1,23 @@
+package linux.java.inputstreamrw;
+
+import java.io.IOException;
+
+/**
+ * packageName    : linux.java.inputstreamrw
+ * fileName       : StandardIOEx1
+ * author         : ipeac
+ * date           : 2023-04-17
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-04-17        ipeac       최초 생성
+ */
+public class StandardIOEx1 {
+    public static void main(String[] args) throws IOException {
+        int input = 0;
+        while ((input = System.in.read()) != -1) {//bufferedInputStream
+            System.out.println("input :" + input + ", (char)input :" + (char) input);
+        }
+    }
+}
