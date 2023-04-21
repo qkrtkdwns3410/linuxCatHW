0a649e034ec70eb787cdb804480546a6d8b06b47
박상준 <psjoon@epart.com>
2023-04-21 23:07:57 +0900

    refactor: TASK 리펙터
    Author: 박상준

--- src/main/java/linux/java/bufferedOutputStream/RandomAccessFileEx2.java
+++ src/main/java/linux/java/bufferedOutputStream/RandomAccessFileEx2.java
@@ -1,6 +1,8 @@
 package linux.java.bufferedOutputStream;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
  * packageName    : linux.java.bufferedOutputStream
@@ -14,7 +16,7 @@ import java.io.IOException;
  * 2023-04-21        ipeac       최초 생성
  */
 public class RandomAccessFileEx2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int[] score = {
                 1, 100, 90, 90,
                 2, 70, 90, 100,
@@ -22,6 +24,22 @@ public class RandomAccessFileEx2 {
                 4, 70, 60, 80,
                 5, 70, 90, 100,
         };
+        try {
+            RandomAccessFile raf = new RandomAccessFile("score.dat", "rw");
+            for (int i = 0; i < score.length; i++) {
+                raf.writeInt(score[i]);
+            }
+            
+            while (true) {
+                System.out.println(raf.readInt());
+            }
+        } catch (EOFException eof) {
+            //readInt() 호출시 더 이상 읽을 내용이 없다면 EOFException이 발생합니다.
+        } catch (IOException e) {
+            e.printStackTrace();
+            
+        }
+        
     }
 }
 
