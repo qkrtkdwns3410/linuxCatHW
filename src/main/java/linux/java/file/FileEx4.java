c2bab30ba43364cf01f7371eb77d904d63be70a8
박상준 <psjoon@epart.com>
2023-04-23 21:53:31 +0900

    chore: 파일공부
    Author: 박상준

--- src/main/java/linux/java/file/FileEx4.java
+++ src/main/java/linux/java/file/FileEx4.java
@@ -1,6 +1,9 @@
 package linux.java.file;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * packageName    : linux.java.file
@@ -15,6 +18,28 @@ import java.io.IOException;
  */
 public class FileEx4 {
     public static void main(String[] args) throws IOException {
-    
+        String currDir = System.getProperty("user.dir");
+        File dir = new File(currDir);
+        
+        File[] files = dir.listFiles();
+        
+        for (int i = 0; i < files.length; i++) {
+            File f = files[i];
+            String name = f.getName();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mma");
+            String attribute = "";
+            String size = "";
+            
+            if (files[i].isDirectory()) {
+                attribute = "DIR";
+            } else {
+                size = String.valueOf(f.length());
+                attribute = f.canRead() ? "R" : " ";
+                attribute += f.canWrite() ? "W" : " ";
+                attribute += f.isHidden() ? "H" : " ";
+            }
+            System.out.printf("%s %3s %6s %s\n", df.format(new Date(f.lastModified())), attribute, size, name);
+            
+        }
     }
 }
