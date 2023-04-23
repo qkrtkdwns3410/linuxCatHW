8a44d7588f8b172113b3e74987e4bf8475671da6
박상준 <psjoon@epart.com>
2023-04-23 21:24:37 +0900

    chore: 파일공부
    Author: 박상준

---
+++ src/main/java/linux/java/file/FileEx4.java
@@ -0,0 +1,20 @@
+package linux.java.file;
+
+import java.io.IOException;
+
+/**
+ * packageName    : linux.java.file
+ * fileName       : FileEx3
+ * author         : ipeac
+ * date           : 2023-04-23
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-04-23        ipeac       최초 생성
+ */
+public class FileEx4 {
+    public static void main(String[] args) throws IOException {
+    
+    }
+}
