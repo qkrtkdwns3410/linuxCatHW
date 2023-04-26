64bc8d2fcaa3008ca3d0a90860cb5e85a6b6e2ac
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-04-26 21:54:38 +0900

    chore: 일일 스트림
    Author: 박상준
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

--- src/main/java/linux/sub/playground/Main3.java
+++ src/main/java/linux/sub/playground/Main3.java
@@ -18,24 +18,26 @@ import java.nio.charset.StandardCharsets;
  */
 public class Main3 {
     public static void main(String[] args) throws IOException {
-        String word = "dkajldjlajd lajdlaj ldjaslj dlaj dal djlaskjdal jlas";
-        //논리적 데이터를 물리적데이터로 변환
+        String word = "asdjlajsdl alsdjlaj ldasljadlasjd laksjd";
+        //word 는 내부적으로 유니코드 문자열로 데이터가 저장되어있으며 해당 데이터의 주소값을 참조합니다.
+        //스트림을 사용하기 위하여는 이진데이터가 필요합니다.
+        // 하지만.. String 클래스에서는 문자데이터의 주소를 참조함과 동시에 해당 유니코드형식 문자열를 이진데이터로 즉시 변환해주는 API를 제공하고 있지 않습니다.
+        // 문자데이터를 토대로 데이터를 바이트 배열을 반환하는 API 는 존재하기에 해당 API를 사용합니다.
         byte[] bword = word.getBytes(StandardCharsets.UTF_8);
-        //물리적 데이터를 가 메모리에 존재하기에 입력받기위해 입력스트림선언
+        // 해당 이진데이터는 메모리에 저장되어있기에 ByteArrayInputStream을 사용합니다.
         InputStream is = new ByteArrayInputStream(bword);
-        //스트리밍이란
+        //스트리밍은
         //1) 적절한 처리단위를 정하여
         byte[] bytes = new byte[4];
-        //2) 조금씩 스트림을 처리하는 행위
+        //2)조금씩 스트림을 처리하는 행위입니다.
         while (true) {
-            //처리단위만큼 입력스트림을 받습니다.
             int len = is.read(bytes);
-            //-1 을 반환한다면 스트림의 종료를 의미합니다.
             boolean eol = len == -1;
             if (eol) {
                 break;
             }
-            //물리적 데이터를 논리적 데이터로 변환하여 콘솔에 출력
+            //저는 콘솔에 입력받은 데이터를 출력할 것이기 때문에 이진데이터를 문자데이터로 변환할겁니다.
+            //디코딩은 인코딩방식과 동일해야합니다 ^^
             String s = new String(bytes, 0, len, StandardCharsets.UTF_8);
             System.out.print(s);
         }
