2b96564dcb813e87bcd8d26b9c9cfb2d1a34b090
박상준 <psjoon@epart.com>
2023-04-27 22:34:19 +0900

    chore: 파일공부
    Author: 박상준

---
+++ src/main/java/linux/java/file/FileEx7.java
@@ -0,0 +1,39 @@
+package linux.java.file;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+/**
+ * packageName    : linux.java.file
+ * fileName       : FileEx6
+ * author         : ipeac
+ * date           : 2023-04-27
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-04-27        ipeac       최초 생성
+ */
+public class FileEx7 {
+    
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("USAGE : java FileEx7 pattern");
+            System.exit(0);
+        }
+        
+        String currDir = System.getProperty("user.dir");
+        
+        File dir = new File(currDir);
+        final String pattern = args[0];
+        
+        String[] files = dir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return false;
+            }
+        })
+    }
+    
+}
