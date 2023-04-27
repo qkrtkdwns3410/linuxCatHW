8fc7bf63e905271efdd78156b74a4139640a30a0
박상준 <psjoon@epart.com>
2023-04-27 22:17:02 +0900

    chore: 파일공부
    Author: 박상준

--- src/main/java/linux/java/file/FileEx6.java
+++ src/main/java/linux/java/file/FileEx6.java
@@ -1,6 +1,8 @@
 package linux.java.file;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 /**
@@ -30,9 +32,44 @@ public class FileEx6 {
             System.exit(0);
         }
         try {
-        
+            findInFiles(dir, keyword);
         } catch (IOException e) {
-            
+            e.printStackTrace();
+        }
+        System.out.println();
+        System.out.println("총 " + found + "개의 라인에서 '" + keyword + "'을/를 발견하였습니다.");
+    }
+    
+    private static void findInFiles(File dir, String keyword) throws IOException {
+        File[] files = dir.listFiles();
+        
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                findInFiles(files[i], keyword);
+            } else {
+                String filename = files[i].getName();
+                String extension = filename.substring(filename.lastIndexOf(".") + 1);
+                extension = "," + extension + ",";
+                if (",java,txt,back,".indexOf(extension) == -1) {
+                    continue;
+                }
+                filename = dir.getAbsolutePath() + File.separator + filename;
+                
+                FileReader fr = new FileReader(files[i]);
+                BufferedReader br = new BufferedReader(fr);
+                
+                String data = "";
+                int lineNum = 0;
+                while ((data = br.readLine()) != null) {
+                    lineNum++;
+                    
+                    if (data.indexOf(keyword) != -1) {
+                        found++;
+                        System.out.println("[" + filename + "(" + lineNum + ")" + "]" + data);
+                    }
+                }//while
+                br.close();
+            }
         }
     }
 }
