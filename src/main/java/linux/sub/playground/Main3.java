2b8c136f5c981f8b063316d40c51d33cf004a0b1
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-05-02 20:26:02 +0900

    chore: 스트림 연습
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

--- src/main/java/linux/sub/playground/Main3.java
+++ src/main/java/linux/sub/playground/Main3.java
@@ -1,5 +1,7 @@
 package linux.sub.playground;
 
+import com.sun.source.tree.TryTree;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,15 +20,18 @@ import java.nio.charset.StandardCharsets;
  */
 public class Main3 {
     public static void main(String[] args) throws IOException {
-        String word = "jdjas alsdklaj ldjlaj asljd a";
-        //String 클래스는 문자열을 리터럴형식으로 저장하는 경우 문자열 상수풀에 해당 데이터를 유니코드형식의 문자열로 저장하게됩니다.
-        // word 변수는 유니코드형식의 문자열의 주소를 참조하게되며, 해당 참조된 데이터를 즉시 이진데이터로 변환해주는 API가 존재한다면  사용하겠지만, 자바에서는 해당 API를 제공해주고 있지 않습니다.
-        // 그렇기에 별도 해당 문자열을 순회하며 byte배열로 변환해주는 별도 API를 사용해서 이진데이터로 변환합니다
+        String word = "ajsld alksdj lajsd jaldsj lasd";
+        /*
+        * 자바에서는 String 클래스가 리터럴로 선언된 경우 문자열 상수 풀에, 객체로 선언된 경우 힙에 유니코드형식의 문자열을 저장합니다.
+        * String 클래스로 선언된 변수 자체는 해당 풀 이나 힙에 저장된 데이터를 참조할 뿐입니다.
+        * 해당 유니코드 형식의 문자열 값 그 자체를 변환하여 컴퓨터가 이해할 수 있는 이진 데이터로 즉시 변환핤 수 있다면 좋겠지만,
+        * String 클래스에서는 제공하지 않고, 별도 byte배열을 새로 생성하여 반환하는 API 는 제공하고 있습니다.
+        * */
         byte[] bword = word.getBytes(StandardCharsets.UTF_8);
-        //이진데이터는 byte 형식으로 메모리에 저장되어있기에 byteArrrayInputStream 으로 스트림을 처리합니다.
+        //이진데이터 자체는 메모리에 저장되어있기에 byteArraYIS 로 데이터 스트림을 처리합니다.
         InputStream is = new ByteArrayInputStream(bword);
-        //스트리밍이란 무엇일까요?
-        // 1. 적절한 처리단위를 정해서
+        //스트리밍이란
+        // 1 . 적절한 처리단위를 정하여
         byte[] bytes = new byte[4];
         //2. 조금씩 스트림을 처리하는 행위를 말합니다.
         while (true) {
@@ -35,7 +40,7 @@ public class Main3 {
             if (eol) {
                 break;
             }
-            //우리는 콘솔에 입력받은 이진데이터를 출력할 예정입니다. System.out.println 에서 String 데이터를 요구하기에 우리가 처음에 인코딩한 방식대로 디코딩해서 출력해줘야합니다.
+            //콘솔에 데이터를 출력할 것이기에 String 클래스로 변환이 필요합니다 + UTF8로 인코딩했기에 같은 방식으로 디코딩해야 올바른 문자열을 볼수잇씁니다.
             String s = new String(bytes, 0, len, StandardCharsets.UTF_8);
             System.out.print(s);
         }
