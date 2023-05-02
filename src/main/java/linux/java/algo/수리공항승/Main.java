a17a6253020c226e2be7a686d907dc7f87aef23f
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-05-02 21:39:54 +0900

    chore: 알고리즘 풀이중
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

---
+++ src/main/java/linux/java/algo/수리공항승/Main.java
@@ -0,0 +1,28 @@
+package linux.java.algo.수리공항승;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * packageName    : linux.java.algo.수리공항승
+ * fileName       : Main
+ * author         : ipeac
+ * date           : 2023-05-02
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-05-02        ipeac       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
