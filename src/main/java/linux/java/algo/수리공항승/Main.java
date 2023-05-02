bbafd08be78d812e50cef5d5cf002f806612fd2e
박상준 <psjoon@epart.com>
2023-05-02 23:49:55 +0900

    chore: https://www.acmicpc.net/problem/1449
    Author: 박상준

--- src/main/java/linux/java/algo/수리공항승/Main.java
+++ src/main/java/linux/java/algo/수리공항승/Main.java
@@ -3,7 +3,8 @@ package linux.java.algo.수리공항승;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
 /**
  * packageName    : linux.java.algo.수리공항승
@@ -17,12 +18,56 @@ import java.util.StringTokenizer;
  * 2023-05-02        ipeac       최초 생성
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-        
-        } catch (IOException e) {
+            String[] firstLine = br.readLine().split(" ");
+            int N = Integer.parseInt(firstLine[0]);//물이 새는 곳의 개수
+            int L = Integer.parseInt(firstLine[1]); // 테이프의 길이
+            String[] leaks = br.readLine().split(" ");
+            Pipe[] pipes = new Pipe[N];
+            for (int i = 0; i < pipes.length; i++) {
+                int number = Integer.parseInt(leaks[i]);
+                pipes[i] = new Pipe(number);
+            }
+            Arrays.sort(pipes, (a, b) -> a.getLeakedPosition() - b.leakedPosition);
+//            System.out.println("pipes = " + Arrays.toString(pipes));
+
+            int index = 0;
+            int count = 0;
+            while (index < N) {
+                int startPosition = pipes[index].getLeakedPosition();
+                while (index < N && pipes[index].getLeakedPosition() - startPosition < L) {
+                    index++;
+                }
+                count++;
+            }
+            System.out.println(count);
+
+        } catch (IOException | NumberFormatException | ArrayIndexOutOfBoundsException e) {
             e.printStackTrace();
         }
-        
+    }
+}
+
+class Pipe {
+    int leakedPosition;
+
+    public Pipe(int leakedPosition) {
+        this.leakedPosition = leakedPosition;
+    }
+
+    public int getLeakedPosition() {
+        return leakedPosition;
+    }
+
+    public void setLeakedPosition(int leakedPosition) {
+        this.leakedPosition = leakedPosition;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Pipe.class.getSimpleName() + "[", "]")
+                .add("leakedPosition=" + leakedPosition)
+                .toString();
     }
 }
