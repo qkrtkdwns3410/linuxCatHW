0b0a4fda58a65a3b37071a14ad10d429d672276b
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-05-04 18:03:35 +0900

    chore: 알고리즘 풀이중
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

---
+++ src/main/java/linux/java/exception/CloseException.java
@@ -0,0 +1,7 @@
+package linux.java.exception;
+
+public class CloseException extends Exception {
+    CloseException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/linux/java/exception/WorkException.java
@@ -0,0 +1,19 @@
+package linux.java.exception;
+
+/**
+ * packageName    : linux.java.exception
+ * fileName       : WorkException
+ * author         : ipeac
+ * date           : 2023-05-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-05-04        ipeac       최초 생성
+ */
+public class WorkException extends Exception {
+    WorkException(String msg) {
+        super(msg);
+    }
+}
+
