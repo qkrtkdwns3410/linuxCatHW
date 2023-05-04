74b8e88737f4724aacb04250fc47d2788f835b4a
박상준 <psjoon@epart.com>
2023-05-05 02:45:19 +0900

    chore: 스트림
    Author: 박상준

--- src/main/java/linux/sub/playground/Main3.java
+++ src/main/java/linux/sub/playground/Main3.java
@@ -1,7 +1,5 @@
 package linux.sub.playground;
 
-import com.sun.source.tree.TryTree;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,16 +20,16 @@ public class Main3 {
     public static void main(String[] args) throws IOException {
         String word = "ajsld alksdj lajsd jaldsj lasd";
         /*
-        * 자바에서는 String 클래스가 리터럴로 선언된 경우 문자열 상수 풀에, 객체로 선언된 경우 힙에 유니코드형식의 문자열을 저장합니다.
-        * String 클래스로 선언된 변수 자체는 해당 풀 이나 힙에 저장된 데이터를 참조할 뿐입니다.
-        * 해당 유니코드 형식의 문자열 값 그 자체를 변환하여 컴퓨터가 이해할 수 있는 이진 데이터로 즉시 변환핤 수 있다면 좋겠지만,
-        * String 클래스에서는 제공하지 않고, 별도 byte배열을 새로 생성하여 반환하는 API 는 제공하고 있습니다.
+        * 자바에서는 String 클래스에서 new 선언은 힙 , 리터럴 ( " " ) 은 문자열 상수풀에 유니코드형식의 문자열로 값을 저장하고, 해당 값을 word 변수가 참조해서 사용합니다.
+        * String 클래스 자체에서는 해당 문자열의 참조한 값을 복사하여 byte 배열로 리턴해주는 방식의 API만 제공하고 있습니다.
+        * 그렇기에 별도의 메모리 공간의 할당하여 새로운 byre 배열을 선언해야합니다.
         * */
         byte[] bword = word.getBytes(StandardCharsets.UTF_8);
-        //이진데이터 자체는 메모리에 저장되어있기에 byteArraYIS 로 데이터 스트림을 처리합니다.
+        //byte 배열은 메모리에 올라가있습니다. byteArris.. 로 스트리밍 처리를 해야합니다.
         InputStream is = new ByteArrayInputStream(bword);
+
         //스트리밍이란
-        // 1 . 적절한 처리단위를 정하여
+        //1 .적절한 처리단위를 정하여
         byte[] bytes = new byte[4];
         //2. 조금씩 스트림을 처리하는 행위를 말합니다.
         while (true) {
@@ -40,7 +38,7 @@ public class Main3 {
             if (eol) {
                 break;
             }
-            //콘솔에 데이터를 출력할 것이기에 String 클래스로 변환이 필요합니다 + UTF8로 인코딩했기에 같은 방식으로 디코딩해야 올바른 문자열을 볼수잇씁니다.
+            //콘솔에 데이터를 출력해서 사용자에게 보이게 할것이기에 sys.out 에서 요하는 String으로 변환
             String s = new String(bytes, 0, len, StandardCharsets.UTF_8);
             System.out.print(s);
         }
