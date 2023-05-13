4a55587fef5eae5839a43ce8890e1fb56c91deed
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-05-13 19:16:18 +0900

    refactor: 테스트 리팩터
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

--- src/main/java/linux/java/inputstreamrw/Tag.java
+++ src/main/java/linux/java/inputstreamrw/Tag.java
@@ -1,5 +1,6 @@
 package linux.java.inputstreamrw;
 
+import java.text.MessageFormat;
 import java.util.StringJoiner;
 
 /**
@@ -14,11 +15,20 @@ import java.util.StringJoiner;
  * 2023-05-10        ipeac       최초 생성
  */
 public class Tag {
-    private int tagNum;
+    private static final int MIN_TAG_VALUE = 1;
+    private static final int MAX_TAG_VALUE = 9;
+    
+    private final int tagNum;
     
     public Tag(int tagNum) {
+        if (!(MIN_TAG_VALUE <= tagNum && tagNum <= MAX_TAG_VALUE)) {
+            String errorMsg = MessageFormat.format("tagNum: {0}이 유효범위({1} ~ {2})에 있지 않습니다", tagNum, MIN_TAG_VALUE, MAX_TAG_VALUE);
+            throw new IllegalArgumentException(errorMsg);
+        }
+        
         this.tagNum = tagNum;
     }
+    
     
     public int getTagNum() {
         return tagNum;
@@ -29,5 +39,10 @@ public class Tag {
         return new StringJoiner(", ", Tag.class.getSimpleName() + "[", "]")
                 .add("tagNum=" + tagNum)
                 .toString();
+    }
+    
+    public static Tag from(String tagNum) {
+        int numValue = Integer.parseInt(tagNum);
+        return new Tag(numValue);
     }
 }
--- src/main/java/linux/java/inputstreamrw/Task.java
+++ src/main/java/linux/java/inputstreamrw/Task.java
@@ -24,15 +24,24 @@ public class Task {
             try {
                 String[] splited = br.readLine().split(" ");
                 FunctionType f = FunctionType.from(splited[0]);//입력컨텍스트에서 생성되었지만, 처리컨텍스스트에 사용자 입력 검증
-                Function function = FunctionFactory.create(f, splited);
-                System.out.println("function = " + function);
-                //입력 컨텍스트가 처리 컨텍스트에 영향을 주면 안됨.
-                //실패 케이스 태그수행시 없으면 실패임
+                Tag tagOrNull = null;
+                if (splited.length == 2) {
+                    tagOrNull = Tag.from(splited[1]);
+                }
+                FunctionTypeAndTag functionTypeAndTag = new FunctionTypeAndTag(f, tagOrNull);
+                
             } catch (RuntimeException e) {
                 e.printStackTrace();
             }
         }
         
+        /*입력 컨텍스트
+        사용자의 입력에 대한 데이터를 입력의 조건 ( 입력을 뭐 공백기준으로 받는지, 엔터로 받는지 등)
+        에 맞춰서 입력을 받는지 검증 하고, 처리 컨텍스트에 필요한 자료의 형태로 데이터를 넘겨주는 역할을 한다.
+        처리 컨텍스트에 필요한 자료의 형태 란?
+         (명령어) (태그) 에 해당하는 객제를 말함. 입력컨텍스트에서는 명령어와 태그 자체만 전달하는거지.
+         해당 객체에 대한 유효성을 고려하지 않는다.
+        */
     }
     
 }
