a8bc8eba4dc6a84e03d697ac85c8dd3ee564b9e3
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-05-25 22:05:26 +0900

    .
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

--- src/main/java/linux/java/numberBaseball/Main.java
+++ src/main/java/linux/java/numberBaseball/Main.java
@@ -1,11 +1,13 @@
 package linux.java.numberBaseball;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  * packageName    : linux.java.numberBaseball
@@ -19,62 +21,104 @@ import java.util.Random;
  * 2023-05-24        ipeac       최초 생성
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Random random = new Random();
-        int randomNumber = random.nextInt(1000);
-        int guessNumber = 0;
+        int randomNumber = random.nextInt(899) + 100;
         
-        try (InputStream is = System.in) {
-            StringBuilder word = new StringBuilder();
-            while (true) {
-                byte[] bytes = new byte[3];
-                int len = is.read(bytes);
-                if (len == -1) {
-                    break;
-                }
-                String str = new String(bytes, 0, len, StandardCharsets.UTF_8);
-                word.append(str);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        BaseBallNumberMaker baseBallNumberMaker = BaseBallNumberMaker.from(randomNumber);
+        BaseGamePlayer baseGamePlayer = BaseGamePlayer.fromConsole();
+        while (true) {
+            System.out.print("숫자를 입력해주세요: ");
+            String guessNumber = baseGamePlayer.writeGuessNumber();
+            System.out.println("guessNumber = " + guessNumber);
         }
-        
-        
     }
     
+    public static class Application {
+        private List<Integer> guessNumber;
+        
+        public Application(List<Integer> guessNumber) {
+            this.guessNumber = guessNumber;
+        }
+        
+        public
+    }
     
     public static class BaseGamePlayer {
-        List<Integer> answerNumbers;
-        List<Integer> guessNumbers;
+        BufferedReader bufferedReader;
         
-        public BaseGamePlayer(List<Integer> answerNumbers) {
-            this.answerNumbers = makeRandomNumber();
-            this.guessNumbers = new ArrayList<>();
+        public BaseGamePlayer(BufferedReader bufferedReader) {
+            this.bufferedReader = bufferedReader;
         }
         
-        private static List<Integer> makeRandomNumber() {
-            List<Integer> list = new ArrayList<>();
-            Random random = new Random();
-            
-            for (int i = 0; i < 3; i++) {
-                list.add(random.nextInt(10));
+        public static BaseGamePlayer fromConsole() {
+            return new BaseGamePlayer(openConsoleBufferedInputStream());
+        }
+        
+        private static BufferedReader openConsoleBufferedInputStream() {
+            return new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        public List<Integer> writeGuessNumber() {
+            List<Integer> arr = new ArrayList<>();
+            try {
+                String inputWord = bufferedReader.readLine();
+                arr = Arrays.stream(converStringToIntArr(inputWord)).boxed()
+                            .collect(Collectors.toUnmodifiableList());
+                return arr;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return list;
+            return arr;
+            
+        }
+        
+        private static int[] converStringToIntArr(String randomNumbers) {
+            int[] converArr = new int[randomNumbers.length()];
+            try {
+                for (int i = 0; i < randomNumbers.length(); i++) {
+                    converArr[i] = Character.getNumericValue(randomNumbers.charAt(i));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return converArr;
         }
     }
     
-    public static class BaseBallGame {
-        BaseGamePlayer baseGamePlayer1;
-        BaseGamePlayer baseGamePlayer2;
+    public static class BaseBallNumberMaker {
         
-        public BaseBallGame(BaseGamePlayer baseGamePlayer1, BaseGamePlayer baseGamePlayer2) {
-            this.baseGamePlayer1 = baseGamePlayer1;
-            this.baseGamePlayer2 = baseGamePlayer2;
+        private final int[] answer;
+        
+        private BaseBallNumberMaker(int[] answer) {
+            if (answer.length != 3) {
+                throw new IllegalArgumentException("3개의 숫자로 하는 게임임 ");
+            }
+            this.answer = answer;
         }
         
-        public void startGame() {
-        
+        public String scoring(String guessNumber) {
+            return null;
         }
+        
+        public static BaseBallNumberMaker from(int randomNumbers) {
+            int[] answerArr = converStringToIntArr(randomNumbers);
+            return new BaseBallNumberMaker(answerArr);
+        }
+        
+        private static List<Integer> converToIntArr(int randomNumbers) {
+            String numberStr = Integer.toString(randomNumbers);
+            int[] converArr = new int[numberStr.length()];
+            try {
+                for (int i = 0; i < numberStr.length(); i++) {
+                    converArr[i] = Character.getNumericValue(numberStr.charAt(i));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return converArr;
+        }
+        
     }
     
 }
