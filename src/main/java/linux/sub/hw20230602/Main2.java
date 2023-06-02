3571edbc6997ee595f56550faf5d91019659e098
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-06-02 21:34:06 +0900

    refactor: 야구게임
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

---
+++ src/main/java/linux/sub/hw20230602/Main2.java
@@ -0,0 +1,45 @@
+package linux.sub.hw20230602;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * packageName    : linux.sub.hw20230602
+ * fileName       : Main2
+ * author         : ipeac
+ * date           : 2023-06-02
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-06-02        ipeac       최초 생성
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        String word = "akjdlasjld kaljdslj asldj as";
+//        byte[] bword = word.getBytes(StandardCharsets.UTF_8);
+        
+        try (OutputStream os = new FileOutputStream("123.txt");
+             BufferedOutputStream bos = new BufferedOutputStream(os, 8192);
+             OutputStreamWriter osw = new OutputStreamWriter(bos, StandardCharsets.UTF_8);
+        ) {
+            osw.write(word);
+            osw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (MyResource myResource = new MyResource();) {
+        }
+    }
+    
+    public void print() {
+    }
+    
+    public static class MyResource implements Closeable {
+        
+        @Override
+        public void close() {
+        
+        }
+    }
+}
