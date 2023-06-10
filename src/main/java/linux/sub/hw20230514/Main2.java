8c6d177e9cd4952acdfbde6d9fd0ec19d19eb1f0
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-06-10 18:59:27 +0900

    소켓
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

--- src/main/java/linux/sub/hw20230514/Main2.java
+++ src/main/java/linux/sub/hw20230514/Main2.java
@@ -16,27 +16,23 @@ import java.nio.charset.StandardCharsets;
  */
 public class Main2 {
     public static void main(String[] args) {
-        String word = "akdklsa lajdals dljals";
-        byte[] bword = word.getBytes(StandardCharsets.UTF_8);
-        
-        try (InputStream is = new ByteArrayInputStream(bword);
+        try (InputStream is = new FileInputStream("test.txt");
              BufferedInputStream bis = new BufferedInputStream(is, 8192);
              InputStreamReader isr = new InputStreamReader(bis, StandardCharsets.UTF_8);) {
-            char[] chars = new char[4];
+            //이진데이터를 스트림에서 읽어오기위한 내부 버퍼가 필요하다
+            char[] chars = new char[512];
+            //스트리밍 처리를 진행한다.
             while (true) {
                 int len = isr.read(chars);
                 if (len == -1) {
                     break;
                 }
-                String str = new String(chars, 0, len);
-                System.out.print(str);
+                //char 자료형의 경우 이미 디코딩이 되어있는 상태이기에 그대로 출력하면된다.
+                String pp = new String(chars, 0, len);
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         
+        }
     }
-    
-
 }
 
---
+++ src/main/java/linux/sub/hw20230610/MainSocket.java
@@ -0,0 +1,52 @@
+package linux.sub.hw20230610;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * packageName    : linux.sub.hw20230610
+ * fileName       : MainSocket
+ * author         : ipeac
+ * date           : 2023-06-10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-06-10        ipeac       최초 생성
+ */
+public class MainSocket {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(7777);
+        System.out.println("대기");
+        Socket socket = serverSocket.accept();
+        System.out.println("연결완료");
+        
+        BufferedInputStream bis = new BufferedInputStream(socket.getInputStream(), 8192);
+        InputStreamReader isr = new InputStreamReader(bis, StandardCharsets.UTF_8);
+        
+        BufferedReader br = new BufferedReader(isr, 8192);
+        
+        /*while (true) {
+            String line = br.readLine();
+            if (line == null) {
+                break;
+            }
+            System.out.println(line);
+        }*/
+        String content = "<h1>Hello world</h1>";
+        OutputStreamWriter osw = new OutputStreamWriter(new BufferedOutputStream(socket.getOutputStream(), 8192), StandardCharsets.UTF_8);
+        BufferedWriter bw = new BufferedWriter(osw, 8192);
+        bw.write("HTTP/1.1 200 OK");
+        bw.newLine();
+        bw.write("Content-Length:" + content.length());
+        bw.newLine();
+        bw.write("Content-Type:application/octec-stream");
+        bw.newLine();
+        bw.newLine();
+        bw.write(content);
+        bw.flush();
+    }
+    
+}
