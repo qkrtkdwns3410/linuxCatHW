aa628afd8165cd8d7cb98208e5f06de6686de177
박상준 <psjoon@epart.com>
2023-06-11 22:54:42 +0900

    feat: 야구게임 Author: 박상준

--- src/main/java/linux/sub/hw202230611/Main.java
+++ src/main/java/linux/sub/hw202230611/Main.java
@@ -12,5 +12,12 @@ package linux.sub.hw202230611;
  * 2023-06-11        ipeac       최초 생성
  */
 public class Main {
-
+    public static void main(String[] args) {
+        CustomArrayList<String> customArrayList = new CustomArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            customArrayList.add("e");
+        }
+        System.out.println("customArrayList.size() = " + customArrayList.size());
+        System.out.println("customArrayList = " + customArrayList);
+    }
 }
