6bf9447bdeca22d4fb6ed45b379f0cf7f9f5f573
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-06-12 21:31:46 +0900

    feat: 배열 addAll() 메서드
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

--- src/main/java/linux/sub/hw202230611/CustomArrayList.java
+++ src/main/java/linux/sub/hw202230611/CustomArrayList.java
@@ -6,21 +6,11 @@ import java.util.StringJoiner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * packageName    : linux.sub.hw202230611
- * fileName       : List
- * author         : ipeac
- * date           : 2023-06-11
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023-06-11        ipeac       최초 생성
- */
 public class CustomArrayList<T> {
     private static final Object[] DEFAULT_ELEMENTS = {};
-    private static Object[] elements;
     private static final int DEFAULT_MAX_CAPACITY = 10;
+    private static Object[] elements;
+    private final int MAX_ARRAY_SIZE = 2100000000;
     private int innerSize = 0;
     
     public CustomArrayList() {
@@ -30,10 +20,10 @@ public class CustomArrayList<T> {
     public CustomArrayList(int capacity) {
         if (capacity == 0) {
             elements = DEFAULT_ELEMENTS;
-        } else if (capacity > 0) {
+        } else if (capacity > 0 && capacity < MAX_ARRAY_SIZE) {
             elements = new Object[capacity];
         } else {
-            throw new IllegalArgumentException("초깃값은 0 혹은 양수여야합니다.");
+            throw new IllegalArgumentException("초깃값은 0 ~ 21억 이하의 정수만 가능합니다. ");
         }
     }
     
@@ -46,17 +36,30 @@ public class CustomArrayList<T> {
         return true;
     }
     
-    public boolean addAll(CustomArrayList<T> customArrayList) {
-        Object[] paramObjects = customArrayList.toObjects();
-        
+    public boolean addAll(CustomArrayList<T> target) {
+        Object[] paramObjects = target.toObjects();
+        ensureCapacity(target.size());// 배열의 안전한 삽입을 위해 공간을 확보
+        System.arraycopy(elements, 0, paramObjects, innerSize, target.size());
+        innerSize += target.size();
         return true;
     }
     
-    private void increaseSize() {
-        copyElements();
+    private void ensureCapacity(int wishSize) {
+        boolean notEnoughSpace = elements.length < wishSize;
+        checkMaxCapacity(wishSize);
+        if (notEnoughSpace) {
+            increaseSize();
+            ensureCapacity(elements.length);
+        }
     }
     
-    private void copyElements() {
+    private void checkMaxCapacity(int wishSize) {
+        if (MAX_ARRAY_SIZE < wishSize) {
+            throw new IllegalStateException("배열의 허용공간을 초과했습니다.");
+        }
+    }
+    
+    private void increaseSize() {
         int increasedSize = elements.length == 0 ? DEFAULT_MAX_CAPACITY : (int) (elements.length * (1.5));
         elements = Arrays.copyOf(elements, increasedSize);
     }
@@ -65,19 +68,18 @@ public class CustomArrayList<T> {
         return innerSize;
     }
     
-    public boolean checkCapacity() {
+    public boolean checkMaxCapacity() {
         return true;
     }
-    public 
     
     private Object[] toObjects() {
-        return Arrays.stream(elements).toArray();
+        return elements;
     }
     
     @Override
     public String toString() {
         return new StringJoiner(", ", CustomArrayList.class.getSimpleName() + "[", "]")
-                .add("elements=" + Stream.of(elements).limit(innerSize).map(Objects::toString)
+                .add("elements=" + Stream.of(elements).limit(size()).map(Objects::toString)
                                          .collect(Collectors.joining(", ", CustomArrayList.class.getSimpleName() + "[", "]")))
                 .toString();
     }
--- src/main/java/linux/sub/hw202230611/Main.java
+++ src/main/java/linux/sub/hw202230611/Main.java
@@ -1,5 +1,8 @@
 package linux.sub.hw202230611;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * packageName    : linux.sub.hw202230611
  * fileName       : List
@@ -13,11 +16,23 @@ package linux.sub.hw202230611;
  */
 public class Main {
     public static void main(String[] args) {
+        // List<String> arr = new ArrayList<>();
+        // for (int i = 0; i < 1000000000; i++) {
+        //     arr.add("e");
+        // }
+        // System.out.println("arr = " + arr);
         CustomArrayList<String> customArrayList = new CustomArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 2; i++) {
             customArrayList.add("e");
         }
+        CustomArrayList<String> customArrayList2 = new CustomArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            customArrayList2.add("a");
+        }
+        customArrayList.addAll(customArrayList2);
         System.out.println("customArrayList.size() = " + customArrayList.size());
         System.out.println("customArrayList = " + customArrayList);
+        System.out.println("customArrayList2.size() = " + customArrayList2.size());
+        System.out.println("customArrayList2 = " + customArrayList2);
     }
 }
