7ddf04e3e114a78e8db87d2af93bad49a9a20c5a
박상준 <psjoon@epart.com>
2023-06-18 01:24:15 +0900

    feat: 리스트

--- src/main/java/linux/sub/hw20230617_2/Q1.java
+++ src/main/java/linux/sub/hw20230617_2/Q1.java
@@ -18,20 +18,24 @@ public class Q1 {
     public static void main(String[] args) throws IOException {
         String word = "안녕하세요 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ";
         byte[] bytes = word.getBytes(StandardCharsets.UTF_8);
-        InputStream is = new BufferedInputStream(new ByteArrayInputStream(bytes), 8192);
-        OutputStream os = new BufferedOutputStream(System.out, 8192);
-        while (true) {
-            try {
-                int len = is.read(bytes);
-                if (len == -1) {
-                    break;
+        try (
+                InputStream is = new BufferedInputStream(new ByteArrayInputStream(bytes), 8192);
+                OutputStream os = new BufferedOutputStream(System.out, 8192);
+        ) {
+            while (true) {
+                try {
+                    int len = is.read(bytes);
+                    if (len == -1) {
+                        break;
+                    }
+                    os.write(bytes, 0, len);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-                os.write(bytes, 0, len);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                os.flush();
             }
-            os.flush();
-            
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
