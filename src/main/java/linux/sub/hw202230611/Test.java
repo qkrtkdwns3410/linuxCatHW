636f7766186ce1ad0c94fb8ed000ec970e44fdf1
박상준 <psjoon@epart.com>
2023-06-22 22:40:25 +0900

    feat: arraylist 완성

--- src/main/java/linux/sub/hw202230611/Test.java
+++ src/main/java/linux/sub/hw202230611/Test.java
@@ -15,43 +15,27 @@ import java.nio.charset.StandardCharsets;
  * 2023-06-17        ipeac       최초 생성
  */
 public class Test {
-    
-    
     public static void main(String[] args) {
-        String word = "안녕하세요";
-        byte[] bword = word.getBytes(StandardCharsets.UTF_8);
+        String word = "HelloWorld";
+        byte[] bytes = word.getBytes(StandardCharsets.UTF_8);
+        OutputStream os = new BufferedOutputStream(System.out, 8192);
         try (
-                InputStreamReader isr = new InputStreamReader(new BufferedInputStream(new ByteArrayInputStream(bword), 8192), StandardCharsets.UTF_8);
-                OutputStreamWriter osw = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream("test1.txt"), 8192), "EUC-KR")
+                InputStream is1 = new BufferedInputStream(new FileInputStream("test1.txt"), 8192);
+                InputStream is2 = new BufferedInputStream(new FileInputStream("test2.txt"), 8192);
+                InputStream is3 = new BufferedInputStream(new FileInputStream("test3.txt"), 8192);
+                InputStream sequence = new SequenceInputStream(is1, new SequenceInputStream(is2, is3))
         ) {
-            char[] chars = new char[1024];
+            byte[] buffer = new byte[1024];
             while (true) {
-                int len = isr.read(chars);
+                int len = sequence.read(buffer);
                 if (len == -1) {
                     break;
                 }
-                osw.write(chars, 0, len);
+                os.write(buffer, 0, len);
             }
-            osw.flush();
+            os.flush();
         } catch (IOException e) {
         
         }
-        try (
-                InputStreamReader isr = new InputStreamReader(new BufferedInputStream(new FileInputStream("test1.txt"), 8192), "EUC-KR");
-                OutputStreamWriter osw = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream("test2.txt"), 8192), StandardCharsets.UTF_8)
-        ) {
-            char[] chars = new char[1024];
-            while (true) {
-                int len = isr.read(chars);
-                if (len == -1) {
-                    break;
-                }
-                osw.write(chars, 0, len);
-            }
-            osw.flush();
-        } catch (IOException e) {
-        
-        }
-        
     }
 }
