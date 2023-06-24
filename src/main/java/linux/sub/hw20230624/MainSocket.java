2c33be7be11ac8c2fb3161e2eb13ae6947b65106
박상준 <psjoon@epart.com>
2023-06-25 01:20:14 +0900

    feat: arraylist 완성

--- src/main/java/linux/sub/hw20230624/MainSocket.java
+++ src/main/java/linux/sub/hw20230624/MainSocket.java
@@ -14,59 +14,62 @@ public class MainSocket {
     public static void main(String[] args) {
         boolean isSequenceEnter = false;
         int sequencialCnt = 0;
-        try {
-            ServerSocket serverSocket = new ServerSocket(7777);
-            Socket socket = serverSocket.accept();
-            OutputStream os = new BufferedOutputStream(System.out, 8192);
-            
-            try (
-                    InputStream is = new BufferedInputStream(socket.getInputStream(), 8192);
-                    OutputStream socketOs = new BufferedOutputStream(socket.getOutputStream(), 8192);
-                    OutputStreamWriter osw = new OutputStreamWriter(socketOs, StandardCharsets.UTF_8);
-                    BufferedWriter bw = new BufferedWriter(osw, 8192);
-            ) {
-                byte[] bytes = new byte[1024];
-                while (true) {
-                    int len = is.read(bytes);
-                    
-                    for (byte aByte : bytes) {
-                        if (aByte == CARRIAGE_RETURN || aByte == LINE_FEED) {
-                            sequencialCnt++;
-
-                        } else {
-                            sequencialCnt = 0;
+        //무한 서버 반복
+        while (true) {
+            try {
+                ServerSocket serverSocket = new ServerSocket(7777);
+                Socket socket = serverSocket.accept();
+                OutputStream os = new BufferedOutputStream(System.out, 8192);
+                
+                try (
+                        InputStream is = new BufferedInputStream(socket.getInputStream(), 8192);
+                        OutputStream socketOs = new BufferedOutputStream(socket.getOutputStream(), 8192);
+                        OutputStreamWriter osw = new OutputStreamWriter(socketOs, StandardCharsets.UTF_8);
+                        BufferedWriter bw = new BufferedWriter(osw, 8192);
+                ) {
+                    byte[] bytes = new byte[1024];
+                    while (true) {
+                        int len = is.read(bytes);
+                        // CRLF 순서를 지켰는지 체크할것
+                        for (byte aByte : bytes) {
+                            if (aByte == CARRIAGE_RETURN || aByte == LINE_FEED) {
+                                sequencialCnt++;
+                            } else {
+                                sequencialCnt = 0;
+                            }
                         }
-                    }
-                    if (sequencialCnt == SPACE_ENTER_CNT) {
-                        isSequenceEnter = true;
-                        break;
-                    }
-                    if (len == -1) {
-                        System.out.println("종료");
-                        break;
-                    }
-                    os.write(bytes, 0, len);
-                    if (isSequenceEnter) {
-                        os.write(ENTER);
-                        bw.write("HTTP/1.1 200 OK");
-                        bw.newLine();
-                        bw.write("Content-Type: text/html");
-                        bw.newLine();
-                        bw.newLine();
-                        bw.write("<h2>Hello World</h2>");
-                        break;
+                        if (sequencialCnt == SPACE_ENTER_CNT) {
+                            isSequenceEnter = true;
+                            break;
+                        }
+                        if (len == -1) {
+                            System.out.println("종료");
+                            break;
+                        }
+                        os.write(bytes, 0, len);
+                        if (isSequenceEnter) {
+                            os.write(ENTER);
+                            bw.write("HTTP/1.1 200 OK");
+                            bw.newLine();
+                            bw.write("Content-Type: text/html");
+                            bw.newLine();
+                            bw.newLine();
+                            bw.write("<h2>Hello World</h2>");
+                            break;
+                        }
+                        os.flush();
+                        bw.flush();
                     }
                     os.flush();
                     bw.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                os.flush();
-                bw.flush();
+                
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
             
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
