c0bf3e171714142e2a71c895aa3008c38b77cb61
qkrtkdwns3410 <qkrtkdwns3410@gmail.com>
2023-07-08 19:04:55 +0900

    feat: 완성
    Signed-off-by: qkrtkdwns3410 <qkrtkdwns3410@gmail.com>

--- src/main/java/linux/sub/hw20230706/Q1.java
+++ src/main/java/linux/sub/hw20230706/Q1.java
@@ -1,9 +1,7 @@
 package linux.sub.hw20230706;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.text.MessageFormat;
+import java.util.*;
 
 /**
  * packageName    : linux.sub.hw20230706
@@ -20,82 +18,118 @@ public class Q1 {
     public static void main(String[] args) {
         Q1 q1 = new Q1();
         System.out.println(Arrays.toString(q1.solution(50237)));
-        System.out.println(Arrays.toString(q1.solution(15000)));
+        // System.out.println(Arrays.toString(q1.solution(15000)));
+        // System.out.println(Arrays.toString(q1.solution(55555)));
     }
     
     public int[] solution(int money) {
-        Bank bank = new Bank(money);
-        int[] answers = bank.exchange();
-        return answers;
+        
+        Money remainMoney = new Money(money);
+        List<Money> monies = new ArrayList<>(List.of(
+                new Money(50_000),
+                new Money(10_000),
+                new Money(5_000),
+                new Money(1_000),
+                new Money(500),
+                new Money(100),
+                new Money(50),
+                new Money(10),
+                new Money(1)
+        ));
+        // monies.sort(new MoneyAscComparator().reversed());
+        Collections.sort(monies);
+        // System.out.println("monies = " + monies);
+        
+        List<Integer> resultCount = new ArrayList<>();
+        for (Money comparableMoney : monies) {
+            if (remainMoney.isSmallerThan(comparableMoney)) {
+                continue;
+            }
+            
+            int countPerMoney = remainMoney.countPer(comparableMoney);
+            Money minusMoney = comparableMoney.times(countPerMoney);
+            
+            remainMoney = remainMoney.minus(minusMoney);
+            
+            resultCount.add(countPerMoney);
+        }
+        
+        return toArray(resultCount);
     }
     
-    public static class Bank {
-        private int money;
-        
-        private Bank(int money) {
-            if (1 > money || money > 1_000_000) {
-                throw new IllegalArgumentException("money 는 1 이상 1,000,000 이하의 자연수만 가능합니다.");
-            }
-            this.money = money;
+    private static int[] toArray(List<Integer> integers) {
+        int[] resultInts = new int[integers.size()];
+        int resultIntsIndex = 0;
+        for (int count : integers) {
+            resultInts[resultIntsIndex] = count;
+            resultIntsIndex++;
         }
-        //!의도는 있으나, 과연 enum 에서 Bank 클래스 위해 정렬을 수행하는 메서드를 짜서 반환하는 형식이 유효한가에 대한 의구심이 든다. -> 시나리오성이 있는가에 대ㅏ한 고민.
-        public int[] exchange() {
-            int[] results = new int[Money.values().length];
-            List<Money> descWons = Money.getMoneyByWonDesc();
-            for (Money money : descWons) {
-                // null 위험 제로 `==` <- 주소비교 연산자로 비교 기본자료형은 실제 값을 자바에서 가지고 있다. 13 이면 그냥 13임.
-                // ENUM 스펙상 ZeroDiv 에러는 발생할수없음
-                int remainer = this.money / money.getWon();
-                this.money %= money.getWon();
-                results[money.getIndex()] = remainer;
-            }
-            return results;
-        }
-        
-        // 필요한가..?
-/*         public static Bank from(int money) {
-            return new Bank(money);
-        } */
+        return resultInts;
     }
     
-    public enum Money {
-        FIFTY_THOUSANDS_BILL(50000, 0),
-        TEN_THOUSANDS_BILL(10000, 1),
-        FIVE_THOUSANDS_BILL(5000, 2),
-        ONE_THOUSAND_BILL(1000, 3),
-        FIVE_HUNDREDS_COIN(500, 4),
-        ONE_HUNDRED_COIN(100, 5),
-        FIFTY_COIN(50, 6),
-        TEN_COIN(10, 7),
-        ONE_COIN(1, 8),
-        ;
+    public static class MoneyAscComparator implements Comparator<Money> {
+        @Override
+        public int compare(Money o1, Money o2) {
+            return o1.value - o2.value;
+        }
         
-        private final int won;
-        private final int index;
+    }
+    
+    public static class Money implements Comparable<Money> {
+        private static final IllegalArgumentException NULL_ARGUMENT_EXCEPTION = new IllegalArgumentException("NULL 값은 허용되지 않습니다");
         
-        Money(int won, int index) {
-            if (won < 1) {
-                throw new IllegalArgumentException("won은 양수만 가능합니다");
+        private final int value;
+        
+        public Money(int value) {
+            if (value < 1 || 1_000_000 < value) {
+                String errorMsg = MessageFormat.format("{0} 은 유효한 금액이 아닙니다.", value);
+                throw new IllegalArgumentException(errorMsg);
             }
-            if (index < 0) {
-                throw new IllegalArgumentException("index 는 0혹은 양수만 가능합니다");
+            this.value = value;
+        }
+        
+        // 금액을 나누다.
+        public int countPer(Money otherMoney) {
+            if (otherMoney == null) {
+                throw NULL_ARGUMENT_EXCEPTION;
             }
-            this.won = won;
-            this.index = index;
+            return this.value / otherMoney.value;
         }
         
-        public int getWon() {
-            return this.won;
+        public Money times(int count) {
+            if (count < 1) {
+                String errorMessage = MessageFormat.format("{0} 값은 1 이상이여야 합니다.", count);
+                throw new IllegalStateException(errorMessage);
+            }
+            return new Money(count * this.value);
         }
         
-        public int getIndex() {
-            return this.index;
+        public Money minus(Money otherMoney) {
+            if (otherMoney == null) {
+                throw NULL_ARGUMENT_EXCEPTION;
+            }
+            return new Money(this.value - otherMoney.value);
         }
         
-        public static List<Money> getMoneyByWonDesc() {
-            List<Money> descWons = Arrays.asList(Money.values());
-            descWons.sort(Comparator.comparingInt(Money::getWon).reversed());
-            return descWons;
+        public boolean isSmallerThan(Money ohterMoney) {
+            if (ohterMoney == null) {
+                throw NULL_ARGUMENT_EXCEPTION;
+            }
+            return this.value < ohterMoney.value;
+        }
+        
+        @Override
+        public int compareTo(Money o) {
+            return o.value - this.value;
+        }
+        
+        @Override
+        public String toString() {
+            return "Money{" +
+                           "value=" + value +
+                           '}';
         }
     }
+    
+    
 }
--- src/main/java/linux/sub/hw20230706/WebServer.java
+++ src/main/java/linux/sub/hw20230706/WebServer.java
@@ -3,19 +3,21 @@ package linux.sub.hw20230706;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Calendar;
 
 public class WebServer {
     public static void main(String[] args) {
-        try (
-                // 서버 소켓은 서버에서 클라이언트의 연결 요청을 기다리는 역할을 한다.
-                // 소켓을 생성함..
-                ServerSocket serverSocket = new ServerSocket(8080);
-        ) {
-            //생성된 소켓은 클라이언트의 요청이 들어올때까지,
-            Socket socket = serverSocket.accept();
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (true) {
+            try (
+                    // 서버 소켓은 서버에서 클라이언트의 연결 요청을 기다리는 역할을 한다.
+                    // 소켓을 생성함..
+                    ServerSocket serverSocket = new ServerSocket(8080);
+            ) {
+                // 생성된 소켓은 클라이언트의 요청이 들어올때까지, 블로킹합니다.
+                Socket socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        
     }
 }
