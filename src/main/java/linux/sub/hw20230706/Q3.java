9da030ce0e374293c89120b0b792b69794379682
박상준 <psjoon@epart.com>
2023-07-12 00:38:14 +0900

    feat: 우테코 3번

--- src/main/java/linux/sub/hw20230706/Q3.java
+++ src/main/java/linux/sub/hw20230706/Q3.java
@@ -1,6 +1,7 @@
 package linux.sub.hw20230706;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
 
 /**
  * packageName    : linux.sub.hw20230706
@@ -17,35 +18,69 @@ public class Q3 {
     public static void main(String[] args) {
         Q3 q3 = new Q3();
         System.out.println(q3.solution("I love you"));
-        // System.out.println(q3.solution("kill nine"));
-        // System.out.println(q3.solution("java is hope"));
-        // System.out.println(q3.solution("cks is god"));
+        System.out.println(q3.solution("kill nine process"));
+        System.out.println(q3.solution("vandal is immutable"));
+        System.out.println(q3.solution("cks is god"));
     }
+    
     public String solution(String word) {
-        String answer = "";
-        
-        return answer;
+        FROG oldFrog = FROG.from(word);
+        FROG newFrog = oldFrog.convertAlpahbet();
+        return newFrog.getWord();
     }
     
     public static class FROG {
         private final IllegalArgumentException NULL_ARGUMENT_ERROR = new IllegalArgumentException("NULL값은 허용되지 않습니다");
-        private final String word;
+        private final char[] words;
         
-        public FROG(String word) {
-            if (word == null) {
+        public FROG(char[] words) {
+            if (words == null) {
                 throw NULL_ARGUMENT_ERROR;
             }
-            if (word.length() < 1 || word.length() > 1000) {
-                String errorMessage = MessageFormat.format("word = {0} 는 1이상 1,000 이하의 문자열이여야합니다", word);
+            if (words.length < 1 || words.length > 1000) {
+                String errorMessage = MessageFormat.format("word = {0} 는 1이상 1,000 이하의 길이이여야합니다", Arrays.toString(words));
                 throw new IllegalArgumentException(errorMessage);
             }
-            this.word = word;
+            this.words = words;
+        }
+        
+        // https://stepbystep1.tistory.com/10
+        public FROG convertAlpahbet() {
+            int upperAsciiCriterion = (int) 'A' + (int) 'Z';
+            int lowerAsciiCritertion = (int) 'a' + (int) 'z';
+            char[] newChars = new char[words.length];
+            
+            for (int index = 0; index < newChars.length; index++) {
+                char word = words[index];
+                if (!Character.isAlphabetic(word)) {
+                    newChars[index] = words[index];
+                    continue;
+                }
+                if (Character.isUpperCase(words[index])) {
+                    newChars[index] = (char) (upperAsciiCriterion - (int) words[index]);
+                } else if (Character.isLowerCase(words[index])) {
+                    newChars[index] = (char) (lowerAsciiCritertion - (int) words[index]);
+                }
+            }
+            return new FROG(newChars);
+        }
+        
+        public String getWord() {
+            StringBuilder stringWord = new StringBuilder();
+            for (char word : this.words) {
+                stringWord.append(word);
+            }
+            return stringWord.toString();
+        }
+        
+        public static FROG from(String words) {
+            return new FROG(words.toCharArray());
         }
         
         @Override
         public String toString() {
             return "FROG{" +
-                           "word='" + word + '\'' +
+                           ", word=" + Arrays.toString(words) +
                            '}';
         }
     }
